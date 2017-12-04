f749f15f154b4837c946113118be66da32749b3a
Jonathan Roberto <jonathan.pontes@hotmail.com>
2017-12-03 23:25:33 -0200

    projeto Hero

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../modelos/hero';
+import { HEROES } from '../modelos/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
